ad5147b703af23332e76d3aff40ea4951d543f6a
Vladislav <v.gaylich@carx-tech.com>
2023-04-11 21:31:24 +0300

    Logger

---
+++ Assets/Scripts/GameCore/Debug/ILogger.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace GameCore.Debug
+{
+    public interface ILogger
+    {
+        void Log(string str);
+        void LogWarning(string str);
+        void LogError(string str);
+    }
+}
